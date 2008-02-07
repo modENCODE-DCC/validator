449f567fd0b3d9d39b9374e59ee6b785d314a258
yostinso <yostinso>
2008-02-07 23:01:26 +0000

    Oops, forgot to add data validation/loading code

---
+++ ModENCODE/Validator/Data/BED.pm
@@ -0,0 +1,72 @@
+package ModENCODE::Validator::Data::BED;
+use strict;
+use Class::Std;
+use Carp qw(croak carp);
+use ModENCODE::Chado::Wiggle_Data;
+
+sub validate {
+  my ($self, $datum) = @_;
+  $datum = $datum->clone();
+  my $success = 1;
+  if (!length($datum->get_value())) {
+    print STDERR "    Warning: No BED file for " . $datum->get_heading() . "\n";
+  }
+  if (!-r $datum->get_value()) {
+    carp "    Cannot find BED file " . $datum->get_value() . " for column " . $datum->get_heading();
+    $success = 0;
+  } else {
+    open FH, '<', $datum->get_value();
+    my $linenum = 0;
+    while (defined(my $line = <FH>)) {
+      $linenum++;
+      next if $line =~ m/^\s*#/; # Skip comments
+      next if $line =~ m/^\s*$/; # Skip blank lines
+      my ($chr, $start, $end, $value) = ($line =~ m/^\s*(\S+)\s+(\d+)\s+(\d+)\s+([-+]?\d+\.?\d*(?:[Ee][-+]?\d+)?)\s*$/);
+      if (!(length($chr) && length($start) && length($end) && length($value))) {
+        carp "    BED file " . $datum->get_value() . " does not seem valid beginning at line $linenum:\n      $line";
+        $success = 0;
+        last;
+      }
+    }
+    close FH;
+  }
+  return $success;
+}
+
+sub merge {
+  my ($self, $datum) = @_;
+  $datum = $datum->clone();
+  if (!length($datum->get_value())) {
+    return $datum;
+  }
+  if (!-r $datum->get_value()) {
+    croak "    Cannot find BED file " . $datum->get_value() . " for column " . $datum->get_heading();
+  }
+  open FH, '<', $datum->get_value();
+  my $linenum = 0;
+  # Build Wiggle object
+  my ($filename) = ($datum->get_value() =~ m/([^\/]+)$/);
+  my $wiggle = new ModENCODE::Chado::Wiggle_Data({
+      'name' => $filename,
+    });
+
+  my $wiggle_data = "";
+  while (defined(my $line = <FH>)) {
+    $linenum++;
+    next if $line =~ m/^\s*#/; # Skip comments
+    next if $line =~ m/^\s*$/; # Skip blank lines
+    my ($chr, $start, $end, $value) = ($line =~ m/^\s*(\S+)\s+(\d+)\s+(\d+)\s+([-+]?\d+\.?\d*(?:[Ee][-+]?\d+)?)\s*$/);
+    if (!(length($chr) && length($start) && length($end) && length($value))) {
+      croak "    BED file " . $datum->get_value() . " does not seem valid beginning at line $linenum:\n      $line";
+    }
+    $wiggle_data .= "$chr $start $end $value\n";
+  }
+  $wiggle->set_data($wiggle_data);
+
+  close FH;
+  $datum->set_wiggle_data($wiggle);
+
+  return $datum;
+}
+
+1;
---
+++ ModENCODE/Validator/Data/WIG.pm
@@ -0,0 +1,11 @@
+package ModENCODE::Validator::Data::WIG;
+use strict;
+use Class::Std;
+use Carp qw(croak carp);
+
+sub validate {
+  my ($self, $datum) = @_;
+  return 0;
+}
+
+1;
