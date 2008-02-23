44add11062a13ed21992322dde1c97c42eb88f3c
yostinso <yostinso>
2008-02-23 02:00:36 +0000

    Fixed lots of bugs in dbEST_acc

--- ModENCODE/Validator/Data/dbEST_acc.pm
+++ ModENCODE/Validator/Data/dbEST_acc.pm
@@ -31,7 +31,8 @@ sub validate {
 
   my @all_results;
   for (my $i = 0; $i < scalar(@ids) + 400; $i += 400) {
-    my $results = $soap_client->run_eSearch({
+    my $term = join(" OR ", @ids[$i..$i+400]);
+    my $results = $soap_client{ident $self}->run_eSearch({
         'eSearchRequest' => {
           'db' => 'nucest',
           'term' => $term,
@@ -45,7 +46,7 @@ sub validate {
     $results->match('/Envelope/Body/eSearchResult/QueryKey');
     my $querykey = $results->valueof();
 
-    my $fetch_results = $soap_client->run_eFetch({
+    my $fetch_results = $soap_client{ident $self}->run_eFetch({
         'eFetchRequest' => {
           'db' => 'nucest',
           'WebEnv' => $webenv,
@@ -65,54 +66,53 @@ sub validate {
     my $datum_success = 1;
     my $result_item;
     foreach my $gbseq (@all_results) {
-      my $found_id = grep { 'gi|' . $id eq $_ } @{$gbseq->{'GBSeq_other-seqids'}->{'GBSeqid'}};
-      if ($id eq $gbseq->{'GBSeq_primary-accession') {
-        $result_item = $gbseq;
+      if ($id eq $gbseq->{'GBSeq_primary-accession'}) {
+          $result_item = $gbseq;
+        }
       }
+      if (!$result_item) {
+        log_error "Couldn't fetch the EST identified by $id from NCBI.";
+        $success = 0;
+        $datum_success = 0;
+      } else {
+
+        # Extract Chado feature information from the SOAP EFetch result
+        my ($dbest_id) = grep { $_ =~ m/^gnl\|dbEST\|/ } @{$result_item->{'GBSeq_other-seqids'}->{'GBSeqid'}}; $dbest_id =~ s/^gnl\|dbEST\|//;
+        my ($genbank_gi) = grep { $_ =~ m/^gi\|/ } @{$result_item->{'GBSeq_other-seqids'}->{'GBSeqid'}}; $genbank_gi =~ s/^gi\|//;
+        my $genbank_acc = $result_item->{'GBSeq_primary-accession'};
+        my ($est_name) = ($result_item->{'GBSeq_definition'} =~ m/^(\S+)/);
+        my $sequence = $result_item->{'GBSeq_sequence'};
+        my $seqlen = length($sequence);
+
+        my $timeaccessioned = $result_item->{'GBSeq_create-date'};
+        my $timelastmodified = $result_item->{'GBSeq_update-date'};
+
+        my ($genus, $species) = ($result_item->{'GBSeq_organism'} =~ m/^(\S+)\s+(.*)$/);
+
+        # Create the feature object
+        my $feature = new ModENCODE::Chado::Feature({
+            'name' => $est_name,
+            'uniquename' => 'dbest:' . $dbest_id,
+            'residues' => $sequence,
+            'seqlen' => $seqlen,
+            'timeaccessioned' => $timeaccessioned,
+            'timelastmodified' => $timelastmodified,
+            'type' => new ModENCODE::Chado::CVTerm({
+                'name' => 'EST',
+                'cv' => new ModENCODE::Chado::CV({ 'name' => 'SO' })
+              }),
+            'organism' => new ModENCODE::Chado::Organism({
+                'genus' => $genus,
+                'species' => $species,
+              }),
+          });
+
+        $datum->set_feature($feature);
+        $datum_hash->{'merged_datum'} = $datum;
+      }
+      $datum_hash->{'is_valid'} = $datum_success;
     }
-    if (!$result_item) {
-      log_error "Couldn't fetch the EST identified by $id from NCBI.";
-      $success = 0;
-      $datum_success = 0;
-    } else {
-
-      # Extract Chado feature information from the SOAP EFetch result
-      my ($dbest_id) = grep { $_ =~ m/^gnl\|dbEST\|/ } @{$result_item->{'GBSeq_other-seqids'}->{'GBSeqid'}}; $dbest_id =~ s/^gnl\|dbEST\|//;
-      my ($genbank_gi) = grep { $_ =~ m/^gi\|/ } @{$result_item->{'GBSeq_other-seqids'}->{'GBSeqid'}}; $genbank_gi =~ s/^gi\|//;
-      my $genbank_acc = $result_item->{'GBSeq_primary-accession'};
-      my ($est_name) = ($result_item->{'GBSeq_definition'} =~ m/^(\S+)/);
-      my $sequence = $result_item->{'GBSeq_sequence'};
-      my $seqlen = length($sequence);
-
-      my $timeaccessioned = $result_item->{'GBSeq_create-date'};
-      my $timelastmodified = $result_item->{'GBSeq_update-date'};
-
-      my ($genus, $species) = ($result_item->{'GBSeq_organism'} =~ m/^(\S+)\s+(.*)$/);
-
-      # Create the feature object
-      my $feature = new ModENCODE::Chado::Feature({
-          'name' => $est_name,
-          'uniquename' => 'dbest:' . $dbest_id,
-          'residues' => $sequence,
-          'seqlen' => $seqlen,
-          'timeaccessioned' => $timeaccessioned,
-          'timelastmodified' => $timelastmodified,
-          'type' => new ModENCODE::Chado::CVTerm({
-              'name' => 'EST',
-              'cv' => new ModENCODE::Chado::CV({ 'name' => 'SO' })
-            }),
-          'organism' => new ModENCODE::Chado::Organism({
-              'genus' => $genus,
-              'species' => $species,
-            }),
-        });
-
-      $datum->set_feature($feature);
-      $datum_hash->{'merged_datum'} = $datum;
-    }
-    $datum_hash->{'is_valid'} = $datum_success;
-  }
-  return $success;
+    return $success;
 }
 
 sub merge {
