7b75865ff65e44d838e497bc2f4948b8e2fa28e5
ellen <ellen>
2012-08-28 23:48:52 +0000

    Updates UIC_File.pm to no longer transfer to UIC. Instead, transfers directly to a fastq directory parallel to the extracted directory. Creates Remote URL and File Transfer Date attributes for the datums.

--- ModENCODE/Validator/Data/UIC_File.pm
+++ ModENCODE/Validator/Data/UIC_File.pm
@@ -16,11 +16,12 @@ use LWP::UserAgent;
 use File::Basename qw(basename);
 use IPC::Open3 qw(open3);
 use IO::Select;
+require Date::Format;
 
 my %cached_fastq_files  :ATTR( :default<{}> );
 my %seen_data           :ATTR( :default<{}> );
 my %transfer_host       :ATTR( :name<transfer_host>,     :default<"74.114.99.142"> );
-my %remote_url_prefix   :ATTR( :name<remote_url_prefix>, :default<"rsync://uberkeley\@74.114.99.142::berkeley/pipeline/"> );
+my %remote_url_prefix   :ATTR( :name<remote_url_prefix>, :default<"http://submit.modencode.org/submit/public/get_file/"> );
 my %local_web_prefix    :ATTR( :name<local_web_prefix>,  :default<"http://submit.modencode.org/submit/public/get_file/"> );
 my %transfer_cmd        :ATTR( :name<transfer_cmd>,      :default<"devel/fetcher.pl"> );
 my %remote_user         :ATTR( :name<remote_user>,       :default<"uberkeley"> );
@@ -39,7 +40,7 @@ sub validate {
   my ($self) = @_;
   my $success = 1;
 
-  log_error "Transferring large files to " . $self->get_transfer_host . ".", "notice", ">";
+  log_error "Transferring large files to local drive.", "notice", ">"; 
   while (my $ap_datum = $self->next_datum) {
     my ($applied_protocol, $direction, $datum) = @$ap_datum;
     next if $seen_data{$datum->get_id}++; # Don't re-update the same datum
@@ -56,66 +57,35 @@ sub validate {
       $success = 0;
       next;
     }
-
-    my ($this_success, $done, $failed, $destination, $destination_size, $log);
+    my $destination ;
     if ($datum_obj->get_value() =~ m|://|) {
-      ($this_success, $success, $done, $failed, $destination, $destination_size, $log) = $self->transfer_file($datum_obj);
-      last unless $success; # Some serious failure means we should stop processing
-      if (!$this_success) {
-        # Local fetch
-        my $url = $datum_obj->get_value();
-        $destination = File::Basename::basename($url);
-        my $uniqid = ($seen_url_filenames{ident $self}->{$destination} ||= 0);
-        if ($uniqid) { $destination = $uniqid . "_" . $destination; }
-        log_error "Fetching remote file from $url to $destination...", "notice", ">";
-        if (-r $destination) {
-          log_error "$destination found locally...overwriting...", "notice";
-        }
-        my $ua = LWP::UserAgent->new();
-        my $res = $ua->mirror($url, $destination);
-        if ($res->is_success) {
-          log_error "Done.", "notice", "<";
-        } elsif ($res->is_error) {
-          my $error_msg = $res->status_line;
-          log_error ("Error retrieving Result File [" . $datum_obj->get_name() . "] from " . $datum_obj->get_value(), "error");
-          log_error ("HTTP Response for $destination was:  " . $error_msg , "error", "<");
-          $success = 0;
-          last;
-        }
-        # Now pass it along to the transfer host from here...
-        my $local_url = $self->get_local_url($destination);
-        my @attributes = $datum_obj->get_attributes;
-        push @attributes, new ModENCODE::Chado::DatumAttribute({
-            'value' => $url,
-            'type' => new ModENCODE::Chado::CVTerm({'name' => 'anyURI', 'cv' => new ModENCODE::Chado::CV({'name' => 'xsd'})}),
-            'name' => 'URL',
-            'heading' => 'File Download URL',
-            'datum' => $datum,
-          });
-        my $new_datum = new ModENCODE::Chado::Data({
-            'heading' => $datum_obj->get_heading,
-            'name' => $datum_obj->get_name,
-            'value' => $local_url,
-            'termsource' => $datum_obj->get_termsource,
-            'type' => $datum_obj->get_type,
-            'attributes' => \@attributes,
-            'features' => $datum_obj->get_features || undef,
-            'wiggle_datas' => $datum_obj->get_wiggle_datas || undef,
-            'organisms' => $datum_obj->get_organisms || undef,
-          });
-        ModENCODE::Cache::update_datum($datum->get_object, $new_datum->get_object);
-        $datum_obj = $new_datum->get_object;
-
-        log_error "Transferring file from local machine ($local_url) to transfer host.", "notice", ">";
-        ($this_success, $success, $done, $failed, $destination, $destination_size, $log) = $self->transfer_file($datum_obj);
-        last unless $success;
-        if (!$this_success) {
-          log_error "Couldn't transfer from local copy to transfer host: $failed.", "error", "<";
-          $success = 0;
-          last;
-        }
-        log_error "Done.", "notice", "<";
+      # Local fetch by default
+      my $url = $datum_obj->get_value();
+      $destination = File::Basename::basename($url);
+      my $uniqid = ($seen_url_filenames{ident $self}->{$destination} ||= 0);
+      if ($uniqid) { $destination = $uniqid . "_" . $destination; }
+      # Download it to the correct path
+      my $destination_dir = $self->get_local_fastq_path() ; 
+      # Make the dir if it doesn't exist
+      unless (-d $destination_dir){
+        mkdir $destination_dir ;
       }
+      my $destination_path = $destination_dir . $destination ;
+      if (-r $destination_path) {
+        log_error "$destination_path found locally...overwriting...", "notice";
+      }
+      my $ua = LWP::UserAgent->new();
+      my $res = $ua->mirror($url, $destination_path);
+      if ($res->is_success) {
+        log_error "Done.", "notice", "<";
+      } elsif ($res->is_error) {
+        my $error_msg = $res->status_line;
+        log_error ("Error retrieving Result File [" . $datum_obj->get_name() . "] from " . $datum_obj->get_value(), "error");
+        log_error ("HTTP Response for $destination_path was:  " . $error_msg , "error", "<");
+        $success = 0;
+        last;
+      }
+            log_error "Done.", "notice", "<";
     } else {
       # It's already a local file
       if ($datum_obj->get_value =~ /TMPID/) {
@@ -129,11 +99,13 @@ sub validate {
     }
     $seen_url_filenames{ident $self}->{$destination}++;
 
+    my $local_url = $self->get_local_url($destination) ; 
+    
     # Update datum to include a pointer to the remote URL
     my $current_time = time();
     my $current_date = Date::Format::time2str("%Y-%m-%d", $current_time, 'GMT');
     $datum_obj->add_attribute(new ModENCODE::Chado::DatumAttribute({
-          'value' => $destination,
+          'value' => $local_url,
           'type' => new ModENCODE::Chado::CVTerm({'name' => 'anyURI', 'cv' => new ModENCODE::Chado::CV({'name' => 'xsd'})}),
           'name' => 'Remote URL',
           'heading' => 'Remote File Location',
@@ -153,141 +125,43 @@ sub validate {
   return $success;
 }
 
-sub parse_response {
-  my $text = shift;
-
-  my ($done) = ($text =~ /^done\t(.*)$/m);
-  if (!defined($done)) { $done = -1; chomp $text; log_error "Couldn't parse $text.", "error"; }
-  my ($failed) = ($text =~ /^failed\t(.*)$/m);
-  my ($destination) = ($text =~ /^destination\t(.*)$/m);
-  my ($destination_size) = ($text =~ /^destination_size\t(.*)$/m);
-  my $log = "";
-  if ($text =~ /^log$/m) {
-    my @text = split(/\n/, $text);
-    my $after_log = 0;
-    foreach my $line (@text) {
-      if ($after_log) {
-        last if ($line !~ /^  /);
-        $log .= $line . "\n";
-      }
-      if ($line =~ /^log$/) { $after_log = 1; }
-    }
+# Returns path to the folder that you should put documents in.
+# If there is at least one /extracted/ in the path, it is a "fastq" folder
+# at the same level as the highest extracted ; otherwise it is a "fastq"
+# folder inside the lowest folder.
+# DOES NOT APPEND DESTINATION ON
+sub get_local_fastq_path {
+  # $ARGV[0] is expected to be the path to the IDF.
+  my ($self) = @_;
+  my $idf_path = $ARGV[0] ;
+  my $fastq_path ;
+  if ($idf_path =~ m/extracted\//){
+    ($fastq_path = $idf_path) =~ s/extracted\/.*//;
+    $fastq_path .= "fastq/" ;
+  } else {
+    ($fastq_path) = ($idf_path =~ m/^(.*\/)/ );
+    $fastq_path .= "fastq/" ;
   }
-  return ($done, $failed, $destination, $destination_size, $log);
+  my $result =$self->canonpath( $fastq_path);
+  return $result ;
 }
 
-sub transfer_file {
-  my ($self, $datum_obj)  = @_;
-  my $success = 1;
-  my $this_success = 1;
-  my $ssh = `which ssh` || '/usr/bin/ssh';
-  chomp($ssh);
-  # SSH to the transfer machine and try to grab the file
-  my $cmd = $self->get_transfer_cmd . " upload";
-  my $url = $datum_obj->get_value();
-  my $command_id = `hostname`;
-  chomp($command_id);
-  $command_id .= "_" . $$;
-  my $failed;
-  my $response = "";
-  {
-    my @args =("-o", "User=" . $remote_user{ident $self}, "-o", "PasswordAuthentication=no", "-o", "IdentityFile=" . $identity_file{ident $self});
-    my $pid = open3(my $sin, my $sout, my $serr, $ssh, @args, $self->get_transfer_host, $cmd);
-    print $sin $url . "\n";
-    print $sin $command_id . "\n";
-    print $sin $self->get_project_id() . "\n";
-    my $s = new IO::Select($sout); #, $serr);
-    while (scalar($s->handles()) > 0) {
-      my @ready = $s->can_read(1);
-      foreach my $h (@ready) {
-        my $buf = <$h>;
-        if (!$buf) {
-          $s->remove($h);
-        } else {
-          if (my ($destination) = ($buf =~ /started\t(.*)/)) {
-            my $remote_destination = $self->canonpath($self->get_remote_url_prefix . "/" . $destination);
-            log_error "Started transferring " . $datum_obj->get_value() . " to $remote_destination.", "notice", ">";
-            $s->remove($sout);
-          }
-          if (($failed) = ($buf =~ /failed\t(.*)/)) {
-            log_error "Failed to transfer " . $datum_obj->get_value() . " to " . $self->get_transfer_host . ": " . $failed . ". Fetching locally.", "warning";
-            $s->remove($sout);
-            $this_success = 0;
-          }
-        }
-      }
-    }
-  }
-
-  my ($done, $destination, $destination_size, $log);
-  if ($this_success) {
-
-    # Wait for transfer to complete
-    $cmd = $self->get_transfer_cmd . " check";
-    while (1) {
-      my @args =("-o", "User=" . $remote_user{ident $self}, "-o", "PasswordAuthentication=no", "-o", "IdentityFile=" . $identity_file{ident $self});
-      open3(my $sin, my $sout, my $serr, $ssh, @args, $self->get_transfer_host, $cmd);
-      print $sin $url . "\n";
-      print $sin $command_id . "\n";
-      print $sin $self->get_project_id() . "\n";
-      my $s = new IO::Select($sout);
-      my $response = "";
-      while (scalar($s->handles()) > 0) {
-        my @ready = $s->can_read(1);
-        foreach my $h (@ready) {
-          my $buf = <$h>;
-          if (!$buf) {
-            $s->remove($h);
-          } else {
-            $response .= $buf;
-          }
-        }
-      }
-      ($done, $failed, $destination, $destination_size, $log) = parse_response($response);
-      if ($done) {
-        # Either success or failure
-        last;
-      } else {
-        # Anything interesting here?
-        log_error "Fetched $destination_size bytes.", "notice";
-      }
-      sleep 5;
-    }
-
-    if ($done == 1) {
-      if (!$failed) {
-        $destination = $self->canonpath($self->get_remote_url_prefix . "/" . $destination);
-        log_error "Successfully transferred $destination_size bytes from " . $datum_obj->get_value() . " to $destination.", "notice", "<";
-      } else {
-        log_error "Failed to transfer file: $failed.", "warning", "<";
-        log_error "Log:", "warning", ">";
-        for my $line (split(/\n/, $log)) {
-          log_error $line, "warning";
-        }
-        log_error "Will try to fetch locally.", "warning", "<";
-        $this_success = 0;
-      }
-    } elsif ($done == -1) {
-      log_error "Lost connection to " . $self->get_transfer_host . ". Please try your validation again or contact help\@modencode.org.", "error";
-      $success = 0;
-      last;
-    }
-  }
-  return ($this_success, $success, $done, $failed, $destination, $destination_size, $log);
-}
-
+# Returns path to where thing can be downloaded from submit site.
+# complains if idf doesn't contain a project id / extracted to
+# figure it out from.
 sub get_local_url {
   my ($self, $destination) = @_;
-  $destination = $self->canonpath($self->get_project_id() . "/extracted/" . $destination);
+  $destination = $self->canonpath($self->get_project_id() . "/fastq/" . $destination);
 
   my $project_id = $self->get_project_id();
   # Tricky workaround to get the actual project path & number
-  $destination =~ s/^.*\/($project_id\/extracted\/.*)/\1/;
+  $destination =~ s/^.*\/($project_id\/fastq\/.*)/\1/;
   if (scalar(split(/\//, $destination)) > 3) {
     # Not running against a real path; shall we fake it?
     log_error "Detected validator not running in a directory like ###/extracted/; can't transfer to " . $self->get_transfer_host . " from here.", "warning";
   }
   $destination = $self->canonpath($self->get_local_web_prefix() . "/" . $destination);
+  #log_error "TODO remove: returning local url>$destination<" ;
   return $destination;
 }
 
